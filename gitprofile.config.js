1bd5ead314d4eb6ab805e0bd94e7ecf21eb112c4
i951 <rebeccatslee@gmail.com>
2024-01-20 12:57:55 -0800

    Add GA id

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -105,7 +105,7 @@ const config = {
   //   limit: 2, // How many posts to display. Max is 10.
   // },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-0R8LKC8RKZ', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
